abcdae27ae6abac37a76704662fcfc2a43bae2f4
Mohamed Gamal <mgtmprog@gmail.com>
2021-05-25 00:21:43 +0200

    can't detect contours

--- Identification/main.py
+++ Identification/main.py
@@ -13,14 +13,16 @@ and_img     = cv2.adaptiveThreshold(and_img, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
 circuit_img = cv2.medianBlur(circuit_img, 3)
 and_img     = cv2.medianBlur(and_img, 3)
 
-circuit_img = cv2.dilate(circuit_img, (7, 7), iterations=2)
-and_img     = cv2.dilate(and_img, (7, 7), iterations=2)
+circuit_img = cv2.erode(circuit_img, (9, 9))
+and_img     = cv2.erode(and_img, (9, 9))
 
 # circuit_img = cv2.Canny(circuit_img, 120, 170)
 # and_img     = cv2.Canny(and_img, 120, 170)
 
 contours_and, hir = cv2.findContours(and_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours_cir, hir = cv2.findContours(circuit_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+print(len(contours_and))
 
 matched = {}
 for c in contours_cir:
@@ -34,11 +36,11 @@ for ret in matched:
   cv2.rectangle(i,(x,y),(x+w,y+h),(255,255,255),2)
   cv2.imshow(str(ret), i)
 
-# img = np.zeros_like(and_img)
-# cv2.drawContours(img, contours[1], -1, (255,255,255), -1)
+# # img = np.zeros_like(and_img)
+# # cv2.drawContours(img, contours[1], -1, (255,255,255), -1)
 
 
-# cv2.imshow("And gate", and_img)
+# cv2.imshow("And gate", circuit_img)
 # cv2.imshow("Count", img)
 
 cv2.waitKey(0)
