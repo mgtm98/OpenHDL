613fe8bd2750506075e0320a3918e1f6f55c1f12
MachineCod <mahmoudn52016@gmail.com>
2021-05-25 05:39:34 +0200

    paint bucket :) filling sucessful

--- Identification/main.py
+++ Identification/main.py
@@ -9,14 +9,19 @@ and_img = cv2.imread('./Images/and.jpg', 0)
 circuit_img = cv2.resize(
     circuit_img, (circuit_img.shape[1]//2, circuit_img.shape[0]//2))
 and_img = cv2.resize(and_img, (and_img.shape[1]//2, and_img.shape[0]//2))
-
+'''
 circuit_img = cv2.adaptiveThreshold(
     circuit_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 5)
 and_img = cv2.adaptiveThreshold(
     and_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 5)
 
-circuit_img = cv2.medianBlur(circuit_img, 3)
+circuit_img = cv2.medianBlur(circuit_img, 5)
 and_img = cv2.medianBlur(and_img, 3)
+'''
+
+#_,circuit_img = cv.threshold(circuit_img,120,175,cv.THRESH_BINARY_INV)
+circuit_img =cv.Canny(circuit_img, 125,175)
+
 
 print(circuit_img.shape)
 
@@ -64,14 +69,21 @@ custom_kernel1 = np.array([[0,0,0,0,0],
 
 circuit_img = cv.dilate(circuit_img,custom_kernel1, iterations=3)
 
-blank = np.zeros_like(circuit_img)
+paintBucket= circuit_img.copy()
+
+cv2.floodFill(paintBucket, None, seedPoint=(0,0), newVal=(255,255,255))
+
+circuit_img = 255- paintBucket + circuit_img
 
 
-circuit_img = cv.Canny(circuit_img , 240 , 255 )
+
+#circuit_img = cv.Canny(circuit_img , 240 , 255 )
 
 
 
-cv2.imshow("And gate", circuit_img)
+
 #cv2.imshow("Count", and_img)
-
+cv2.imshow("Circuit", circuit_img)
+cv2.imshow("filling", 255-paintBucket)
 cv2.waitKey(0)
+cv.destroyAllWindows()
