4625131b403078e1977b32941da9029174e1db50
Mohamed Gamal <mgtmprog@gmail.com>
2021-06-16 12:35:36 +0200

    debugging lines are added

--- ImageProcessing/identify.py
+++ ImageProcessing/identify.py
@@ -7,16 +7,28 @@ import pprint
 import os
 
 pp = pprint.PrettyPrinter(indent=4)
+show = cv2.imshow
+wait = cv2.waitKey
 
 def extract_circuit(path):
     img = cv2.imread(path)                                    # load the circuit colored image 
+    show("original image", img)
+    wait()
+
     grey_img = convert_to_grey(img)                           # convert image to grey scale
+    show("grey image", grey_img)
+    wait()
+    
     thresh = get_thershold(grey_img)                          # convert it to binary image
+    show("binary image", thresh)
+    wait()
 
     gates = copy(thresh)                                      # we will fill the gates in this image
     wires = copy(thresh)                                      # we will operate on wires in this image
 
     fill(gates)                                               # fill the gates to get the contours arround them
+    show("gates_filled", gates)
+    wait()
 
     wires = cv2.bitwise_or(cv2.bitwise_not(gates), wires)     # the binary image with the gates filled()
     t = copy(wires)                                           # temp image of binary image with gate filled
@@ -25,19 +37,20 @@ def extract_circuit(path):
     wires = t - wires                                         # get the wires only with some noise
     wires = erode(wires, (3, 3))                              # remove the noise
     wires = dilate(wires, (3, 3))
-
+    show("wires", wires)
+    wait()
 
     # remove further noise in the wires by detecting the low area contours which resembles the noise
     wires_cont = get_contours(wires)
-    # print("wires", len(wires_cont))
+    print("wires", len(wires_cont))
 
-    for c in wires_cont:
-        if get_contour_area(c) < 50: draw_contour(wires, c, color=BLACK, fill=True)
+    # for c in wires_cont:
+    #     if get_contour_area(c) < 50: draw_contour(wires, c, color=BLACK, fill=True)
     wires = erode(wires, (3, 3))
 
     borders = get_edges(gates)                                # get the borders of the filled gates image
     img_cont = reduce_cont(get_contours(borders))             # extract the contours to operate arround them
-    # print("Number of contours for gates", len(img_cont))      # display number of contours (for debuging) 
+    print("Number of contours for gates", len(img_cont))      # display number of contours (for debuging) 
 
     gates_meta_data = {}
 
